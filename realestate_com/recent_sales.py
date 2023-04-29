f9280024897df8a8ce8ea8cc64b8b40053947014
lawrencefchan <lawrence.fchan@gmail.com>
2023-04-29 23:16:41 +1000

    working recent sales scrape and db write

--- realestate_com/recent_sales.py
+++ realestate_com/recent_sales.py
@@ -1,25 +1,28 @@
 # %%
 
 import pandas as pd
+from datetime import datetime
+
 from bs4 import BeautifulSoup
+
 import undetected_chromedriver as uc  # anti-bot selenium patch
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import (
+    TimeoutException,
+    ElementNotVisibleException,
+    ElementNotSelectableException
+)
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = uc.Chrome()
-
-home = 'https://www.realestate.com.au/auction-results/'
-states = ['nsw', 'qld', 'sa', 'wa', 'nt', 'act', 'tas']
-
-url = home + states[0]
-# url = 'https://www.domain.com.au/auction-results/sydney/'
-url = 'https://www.realestate.com.au/auction-results/nsw'
-driver.get(url)
-
-# %% state profile
 
 def get_state_profile(driver):
     '''
-    Parses the 
+    Parses latest auction and sales results.
+
+    Todo:
+    * parse domain.com (www.domain.com.au/auction-results/sydney)
+
     '''
 
     def find_sibling_by_text(driver, text: str):
@@ -27,9 +30,11 @@ def get_state_profile(driver):
         Searches elements that contain a given string and returns text of
         the preceding sibling.
         '''
-        xpath_str = f"//*[contains(text(), '{text}')]/preceding-sibling::p[1]"
-        value = driver.find_element(By.XPATH, xpath_str).text
 
+        xpath_str = f"//*[contains(text(), '{text}')]/preceding-sibling::p[1]"
+
+        # value = driver.find_element(By.XPATH, xpath_str).text
+        value = fluent_wait(driver, By.XPATH, xpath_str).text
         return int(value.replace(',', ''))
 
     properties = [
@@ -43,14 +48,87 @@ def get_state_profile(driver):
 
     profile = {p: find_sibling_by_text(driver, p) for p in properties}
 
-    cr = driver.find_element(By.XPATH, "//p[@data-testid='number-pie']").text
-    profile['clearance rate'] = int(cr.replace('%', '')) * 0.01
+    # get clearance rate
+    try:  # NA when no houses are auctioned (fluent_wait will timeout)
+        cr = fluent_wait(driver, By.XPATH, "//p[@data-testid='number-pie']").text
+        profile['clearance rate'] = int(cr.replace('%', '')) * 0.01
+    except TimeoutException:
+        profile['clearance rate'] = None
+
+    # get date last updated
+    t = fluent_wait(driver, By.XPATH, "//p[@data-testid='clearance-rate-updated']").text
+    t = t.replace('Updated ', '')[:-5]  # [-5] drops timezone info
+
+    t = datetime.strptime(t, "%a %d %b %H:%M %p") \
+                .replace(year=datetime.now().year)  # REA updated date excludes year
+
+    profile['updated'] = t.date()  # keep date, drop time
 
     return profile
 
 
-get_state_profile(driver=driver)
+def profile_all_states(driver):
+    home = 'https://www.realestate.com.au/auction-results/'
+    states = ['nsw', 'qld', 'sa', 'wa', 'nt', 'act', 'tas']
 
+    output = {}
+
+    print('Profiling ', end='')
+    for s in states:
+        print(s, end=', ')
+        url = home + s
+        driver.get(url)
+
+        d = get_state_profile(driver=driver)
+
+        output[s] = d
+
+    print('done')
+
+    return output
+
+
+def munge_profile_output(d):
+    '''
+    Processes recent sales results and returns a pandas df.
+    '''
+
+    df = pd.DataFrame(d).T
+
+    df.index.name = 'state'
+    df = df.reset_index()
+    df.columns = [i.lower().replace(' ', '_') for i in df.columns]
+
+    df['queried'] = datetime.today().date()
+
+    return df
+
+
+def fluent_wait(driver, *wait_args):
+    '''
+    wait_args: same args as find_element, e.g. (By.XPATH, "//p[@data='clearance-']")
+    '''
+    ignore = [ElementNotVisibleException, ElementNotSelectableException]
+    wait = WebDriverWait(driver, timeout=6, poll_frequency=0.1, ignored_exceptions=ignore)
+    element = wait.until(EC.element_to_be_clickable((wait_args)))
+    return element
+
+
+if __name__ == "__main__":
+    
+    driver = uc.Chrome()
+    driver.minimize_window()
+
+    # # --- debugging
+    # url = 'https://www.realestate.com.au/auction-results/wa'
+    # driver.get(url)
+    # d = get_state_profile(driver)
+
+    d = profile_all_states(driver)
+    df = munge_profile_output(d)
+
+
+# %%
 
 
 # %% get suburb links (bs4 is faster)
