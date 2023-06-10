1812d25640650f1daf998d3f4d2d0f9946202c8b
lawrencefchan <lawrence.fchan@gmail.com>
2023-06-11 01:09:13 +1000

    nicer plot

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -11,14 +11,29 @@ def plot_recent_sales(df):
 
     plot_var = 'clearance_rate'
     # plot_var = 'private_sales'
-    ax = df.pivot_table(
-        index='updated',
-        columns='state',
-        values=plot_var) \
-            .drop(['wa', 'tas', 'nt'], axis=1) \
-            .plot(marker='.')
+
+    d = df.pivot_table(
+            index='updated',
+            columns='state',
+            values=plot_var) \
+                .drop(['wa', 'tas', 'nt'], axis=1)
+
+    d = d.set_index(pd.to_datetime(d.index))
+
+    # plot lines (need uniform timeseries for nice xticklabels)
+    ax = d.resample('1D').mean().interpolate().plot()
+
+    h, l = ax.get_legend_handles_labels()  # get legend for lines
+    clrs = [l.get_color() for l in ax.lines]  # get colors for lines
+
+    # plot dots
+    d.plot(ax=ax, ls='', marker='.', ms=8, legend=False)
+    ax.legend(h, l)  # drop legend for dots
+    for i, l in enumerate(ax.get_lines()):  # set color for dots
+        l.set_color(clrs[i%len(clrs)])
+
     ax.set_title(plot_var)
-    ax.grid()
+    # ax.grid()
 
 
 if __name__ == "__main__":
