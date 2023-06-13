ca7752987fd508e93bfacb394e6714f232daf8df
lawrencefchan <lawrence.fchan@gmail.com>
2023-06-14 00:24:02 +1000

    plot refactoring, wip analysis

--- analysis/rea_historicals.py
+++ analysis/rea_historicals.py
@@ -12,6 +12,8 @@ import pandas as pd
 import plotly.express as px
 
 from utils.sqlite_utils import read_historical_trends
+from utils.plot_utils import plotly_geoplot
+from utils.general_utils import get_suburb_geom
 
 
 def plot_historical_yield(df=None):
@@ -25,9 +27,11 @@ def plot_historical_yield(df=None):
         columns='ownership_type', values='median')
     df['yield'] = df['rent'] * 52 / df['buy']
     df['yield_mv_avg'] = df['yield'].rolling(window=12).mean()
+    df = df.drop(['buy', 'rent'], axis=1).stack().rename('value') \
+        .reset_index()
 
     fig = px.line(
-        df.drop(['buy', 'rent'], axis=1).stack().rename('value').reset_index(),
+        df,
         x='yr_ended',
         y='value',
         color='suburb',
@@ -37,4 +41,40 @@ def plot_historical_yield(df=None):
     fig.for_each_annotation(lambda x: x.update(text=x.text.split('=')[-1]))
     fig.show("browser")
 
+# --- spatial plot median
+try:
+    suburb_geom
+except NameError:
+    suburb_geom = get_suburb_geom().drop(['Postcode', 'Region'], axis=1)
+    suburb_geom.columns = [_.lower() for _ in suburb_geom.columns]
+    suburb_geom['suburb'] = suburb_geom['suburb'].str.lower() \
+        .str.replace(' ', '-')
 
+df = read_historical_trends('prices_volumes')
+df['yr_ended'] = pd.to_datetime(df['yr_ended'])
+
+df = df[(df['ownership_type'] == 'buy') &
+        (df['n_beds'] == 0) &
+        (df['dwelling_type'] == 'house')]
+# get latest available data
+group_cols = ['suburb', 'dwelling_type', 'n_beds']
+df = df.loc[df.groupby(group_cols)['yr_ended'].idxmax()]
+drop_cols = [
+    'volume',
+    'yr_ended',
+    'ownership_type',
+    'dwelling_type',
+    'n_beds',
+    'last_queried']
+df = suburb_geom.merge(
+    df.drop(drop_cols, axis=1),
+    how='inner', on='suburb') \
+    .set_index('suburb')
+# %%
+# plotly_geoplot(df.drop(['vaucluse', 'bellevue-hill', 'longueville']), plot_col='median')
+
+# %%
+
+df.reset_index().sort_values('median').plot.bar(x='suburb', y='median')
+
+px.plot()
---
+++ utils/general_utils.py
@@ -0,0 +1,39 @@
+# %%
+
+import os
+import pandas as pd
+import geopandas as gpd
+
+
+def get_suburb_geom():
+    '''
+    Get df of suburb names and their respective boundaries
+    
+    NOTE:
+    - reading the .shp file is slow
+    - need geopandas to read .shp
+    - .shp needs to be converted to geojson to be plotted with plotly
+
+    TODO:
+    - handle missing suburbs
+    - check if geojson is any faster/easier to use
+    '''
+
+    # NSW Localities SHP GDA20(ZIP):
+    # https://data.gov.au/dataset/ds-dga-91e70237-d9d1-4719-a82f-e71b811154c6/details
+    df_geom = gpd.read_file("./test_data/GDA2020/nsw_localities.shp")
+
+    postcodes = pd.read_csv('postcodes-suburbs-regions.csv')
+
+    # len(set(postcodes['Suburb']) - set(df_geom['LOC_NAME']))
+
+    suburb_geom = df_geom[['LOC_NAME', 'geometry']] \
+        .rename({'LOC_NAME': 'Suburb'}, axis=1) \
+        .merge(postcodes, how='inner', on='Suburb')
+
+    # simplify geometry accuracy
+    accuracy = 50  # meters - 50m seems to be a good tradeoff
+    suburb_geom['geometry'] = suburb_geom.to_crs(suburb_geom.estimate_utm_crs()) \
+        .simplify(accuracy).to_crs(suburb_geom.crs)
+
+    return suburb_geom
